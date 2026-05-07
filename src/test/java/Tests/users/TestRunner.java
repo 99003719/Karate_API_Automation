1cf38aae488224d61fe0bfe6ef4d0a10c93de033
99003719 <aishwaryapuranik97@gmail.com>
2026-05-07 16:15:12 +0530

    Code refactor

--- src/test/java/Tests/users/TestRunner.java
+++ src/test/java/Tests/users/TestRunner.java
@@ -6,8 +6,13 @@ class TestRunner {
     
     @Karate.Test
     Karate testUsers() {
-        return Karate.run("Add_item_non_existing_id_and_validate_the_added_item" +
-                "").relativeTo(getClass());
+        return Karate.run("Add_item_existing_id",
+                "Add_item_non_existing_id",
+                "Add_item_with_missing_info",
+                "Filter_by_id",
+                "Get_all_menu_items",
+                "Validate_recent_added_item_is_present_in_the_inventory"
+                ).relativeTo(getClass());
     }
 
 }
